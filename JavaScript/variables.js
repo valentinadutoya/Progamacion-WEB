52d2ca2792a501caf4b1613384726bc699c72091
valentinadutoya <laravalentinadutoya2019@gmail.com>
2024-04-24 16:45:06 -0300

    trabajo terminado

---
+++ JavaScript/variables.js
@@ -0,0 +1,6 @@
+var name = 'valentina' // con la palabra reservada var podemos definir variables 
+var number = 20 
+var boolean = true 
+// la palabra reservada var para definir variables no se utiliza mas debido a su alto alcance en cualquier
+
+
