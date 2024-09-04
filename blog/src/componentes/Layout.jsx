a0e2bcad69c20ff702fbc4e36c00f70fe71a0f25
valentinadutoya <laravalentinadutoya2019@gmail.com>
2024-09-04 17:06:47 -0300

    no hice nada

---
+++ blog/src/componentes/Layout.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+const Layout = () => {
+  return (
+    <div>
+      <header>
+        <h1>Header</h1>
+        {/* Puedes agregar aquí la navegación u otro contenido */}
+      </header>
+
+      <main>
+        <h2>Main Content</h2>
+        {/* Aquí puedes agregar el contenido principal de la página */}
+      </main>
+
+      <footer>
+        <p>Footer</p>
+        {/* Aquí puedes agregar el contenido del pie de página */}
+      </footer>
+    </div>
+  );
+}
+
+export default Layout;
